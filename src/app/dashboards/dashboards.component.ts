1cb55b54028e8b57fd2124576b9065cf3659b5b3
ycarmi <37307868+ycarmi@users.noreply.github.com>
2018-07-31 16:46:50 +0300

    pie chart
    yehoooooo

--- src/app/dashboards/dashboards.component.ts
+++ src/app/dashboards/dashboards.component.ts
@@ -15,15 +15,16 @@ import { stat } from 'fs';
 export class DashboardsComponent implements OnInit {
   chart= [];
   allData =[];
+  numberOfPoints = this.allData.length;
+
   year = [2000,2001,2002,2003,2004,2005,2006,2007,
     2008,2009,2010,2011,2012,2013,2014,2015,2016,2017];
   redPoints2   : StreetPoints[] = [];
   orangePoints2 : StreetPoints[] = [];
   yellowPoints2 : StreetPoints[] = [];
 
-  constructor(private _streetService:StreetService, private _streetpointsservice:StreetPointsService ) { }
-  
-  /* subscribeToObs(jsonpoints) {
+  constructor(private _streetpointsservice:StreetPointsService ) { }
+   subscribeToObs(jsonpoints) {
     jsonpoints.forEach(element => {
      var statuses = element.statusOverTime;
      var status = statuses[statuses.length-1].trafficStatus;
@@ -35,10 +36,10 @@ export class DashboardsComponent implements OnInit {
        this.yellowPoints2.push(element);
      }
    });
-  } */
-  async foo() {// insert streets data to a variable  
+  } 
+  async subscibeToService() {// insert streets data to a variable  
     let res = await this._streetpointsservice.getStreetPoints();
-    /* this.subscribeToObs(res); */
+    this.subscribeToObs(res); 
     this.subscribe(res);
   }
   async subscribe(points){
@@ -49,31 +50,55 @@ export class DashboardsComponent implements OnInit {
   }
   trafficCapacityPerYear(){
     let yearsSum= [];
+   
+    // Filling in zeros for summing.
     for(let i = 0;i<18;i++)
      yearsSum[i] = 0;
-
+    /**
+     * For each year we add to it each point's traffic capacity ratio
+     */
     this.allData.forEach(dataPoint =>{
      let status = dataPoint.statusOverTime;
      for(let i =0;i<status.length;i++){
        yearsSum[i] +=status[i].trafficCapacityRatio;
      }
-
    })
    let numberOfPoints = this.allData.length
    let yearsAverage = [];
+   // Getting the average for each year
    yearsSum.forEach(year =>{
      yearsAverage.push(year/numberOfPoints)
    })
+
+   // Rounding the averages for 3 decimal points 
    yearsAverage = yearsAverage.map(average => average.toFixed(3));
    return yearsAverage
   }
+  redPointsPercentage(){
+    let redPointsLength = this.redPoints2.length;
+    return((2775/redPointsLength)*100*100);
 
+  }
+  yellowPointsPercentage(){
+    let yellowPointsLength = this.yellowPoints2.length;
+    return (2775/yellowPointsLength)*100*100;
+
+ 
+  }
+  orangePointsPercentage(){
+    let orangePointsLength = this.orangePoints2.length;
+     let orangePercentage = (2775/orangePointsLength)*100*100; 
+    
+    return orangePercentage;
+  }
    ngOnInit() {
 /*       this.foo(); 
- */     Promise.all([this.foo()])
+ */    
+      Promise.all([this.subscibeToService()])
      .then(()=>{       
        
       let yearsAverage = this.trafficCapacityPerYear();
+      let redPercent = this.redPointsPercentage();
 
       this.chart = new Chart('canvas', {
         type: 'line',
@@ -101,6 +126,36 @@ export class DashboardsComponent implements OnInit {
           }
         }
       }); 
+      this.chart = new Chart('pieChart', {
+        type: 'pie',
+        data: {
+           labels: ['Red','Yellow','Orange'],
+           datasets: [
+            { 
+              data: [
+                this.redPointsPercentage(),
+                this.yellowPointsPercentage(),
+                this.orangePointsPercentage(),
+              ],
+              backgroundColor:[
+                'red','yellow','orange'
+              ],
+              borderColor: "#3cba9f",
+              fill: false
+            },
+          ]
+        },
+      
+      }); 
+       
+
+
+      
+      
+
+
+      
+     
 
      })
 
